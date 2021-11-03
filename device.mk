dd4171e786e45fa26680595ec3e717857d73d6d9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-03 15:04:19 +0530

    RMX3031: Add touchpanel keylayout for DT2W
    * touch driver dispatches KEY_F4 (62) keycode instead of KEY_WAKEUP
    for dt2w event, intercept this and dispatch wakeup event to the
    framework to fix dt2w wakeup

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/Image.gz:kernel
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl
+
 # Screen density
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
