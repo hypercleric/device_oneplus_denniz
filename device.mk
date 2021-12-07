883ac743f7e96242b18e99e60a4d80fd1d5d1101
Nishant Kumar <www.rajsonu13@gmail.com>
2021-12-07 13:20:34 +0530

    RMX3031: Import OPFiles port by meo-ify
    * From :- https://t.me/Meoify/433

--- device.mk
+++ device.mk
@@ -119,6 +119,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nxp.conf
 
+PRODUCT_PACKAGES += \
+    OPFiles
+	
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/OPFiles/privapp-permissions-op-files.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-files.xml \
+
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlay \
