40d9e8e8a7493412ccf8ed18fe57eb79b1889dd4
hypercleric <kumaradisslbya@gmail.com>
2022-01-12 08:38:09 +0000

    Ancientfy

--- ancient_denniz.mk
+++ ancient_denniz.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/denniz/device.mk)
 
 # Inherit some common Pixel-Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_denniz
+PRODUCT_NAME := ancient_denniz
 PRODUCT_DEVICE := denniz
 PRODUCT_BRAND := oneplus
 PRODUCT_MODEL := oneplus Nord 2 5G
@@ -37,12 +37,7 @@ IS_PHONE := true
 ANCIENT_GAPPS := true
 
 # Official-ify
-Elixir_MAINTAINER := hypercleric
-IS_PHONE := true
-CUSTOM_BUILD_TYPE := OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+ANCIENT_OFFICIAL := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Build info
