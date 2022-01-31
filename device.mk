e886d3ed8100f0f5104172574dab337b1cfb9dad
hypercleric <kumaradisslbya@gmail.com>
2022-01-31 07:38:33 +0000

    denniz : switch to source build kernel *using prebuilt dtb and dtbo *chnage clang version

--- device.mk
+++ device.mk
@@ -86,10 +86,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mt6893.rc
 
-# Copy the kernel from the prebuilts directory.
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/Image.gz:kernel
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl
